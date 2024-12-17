5f86b0eb570dd34a8ef5e63a8d4dd1eca4213759
vimosanan <vimosanana.93@gmail.com>
2024-12-17 01:15:53 -0600

    Implement ViewModel Test  - add dependencies  - add test case for country view model

---
+++ app/src/test/java/com/example/countrylistapp/CountriesViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.example.countrylistapp
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.example.countrylistapp.data.CountriesRepository
+import com.example.countrylistapp.model.Country
+import com.example.countrylistapp.model.Currency
+import com.example.countrylistapp.model.Language
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.*
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CountriesViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    private lateinit var mockRepository: CountriesRepository
+
+    private lateinit var viewModel: CountriesViewModel
+
+    @Before
+    fun setUp() {
+        mockRepository = mock()
+
+        Dispatchers.setMain(testDispatcher)
+
+        viewModel = CountriesViewModel(mockRepository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `fetchCountries should call repository's fetchCountries`() = runTest {
+        viewModel.fetchCountries()
+
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        verify(mockRepository).fetchCountries()
+    }
+
+    @Test
+    fun `countries LiveData emits data from repository`() = runTest {
+        val mockCountries = listOf(
+            Country(
+                capital = "Kabul",
+                code = "AF",
+                currency = Currency("Afghan afghani", "AFN", "؋"),
+                flag = "https://restcountries.eu/data/afg.svg",
+                language = Language("Pashto", "ps"),
+                name = "Afghanistan",
+                region = "AS"
+            )
+        )
+
+        val liveData = MutableLiveData<List<Country>>()
+        liveData.value = mockCountries
+
+        whenever(mockRepository.getCountries()).thenReturn(liveData)
+
+        val viewModel = CountriesViewModel(mockRepository)
+
+        val observedCountries = mutableListOf<List<Country>>()
+        viewModel.countries.observeForever { countries ->
+            observedCountries.add(countries)
+        }
+
+        assertEquals(1, observedCountries.size)
+        assertEquals(mockCountries, observedCountries[0])
+    }
+}
